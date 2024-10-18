4531af7bdf3043d0b3573340708f859ae2c0b7ce
nineakhil <ninekahil@gmail.com>
2024-10-18 02:16:57 -0700

    first commit

---
+++ firstweb/src/Details.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Details() {
+  return (
+    <>
+    {/* <div>Know ME</div> */}
+    <h1>About Us</h1>
+    <h6>Hi! My Name is Akhil.
+        This is my first ever dynamic page that i have deployed. 
+    </h6>
+    </>
+    
+
+  )
+}
+
+export default Details
